c01e9619eebbae30f2af42a0f60b2f63f9f8012c
Pavlo Varlamov <Pavlo.Varlamov@ext.us.panasonic.com>
2023-12-15 10:08:18 +0200

    Add verification steps

--- tests/first/test_open_page.py
+++ tests/first/test_open_page.py
@@ -1,8 +1,6 @@
-import string
-
 from pages.accout_register_page import AccountRegisterPage
 from pages.home_page import HomePage
-from utils.string import random_string
+from utils.random_values import random_string, random_number
 
 
 class TestHomePage:
@@ -12,16 +10,18 @@ class TestHomePage:
         assert hp.logo_img().is_displayed()
 
     def test_search(self, browser):
+        text = 'Something'
         hp = HomePage(browser)
-        hp.search("Something")
-        assert True
+        hp.search(text)
+        assert text in hp.result_label().text
 
     def test_register_user(self, browser):
+        text = 'Ваш обліковий запис успішно створено!'
         hp = HomePage(browser)
         hp.open_registration_form()
         arp = AccountRegisterPage(browser)
         email = f'{random_string()}@gmail.com'
-        arp.fill_the_form(firstname='Pavlo', lastname='V', email=email, phone=random_string(chars=string.digits),
+        arp.fill_the_form(firstname='Pavlo', lastname='V', email=email, phone=random_number(),
                           address='1 New Ave', city='Lviv', country='Ukraine', region="L'vivs'ka Oblast'",
                           password='test', confirm_password='test', agree=True, submit=True)
-        assert True
+        assert hp.result_label().text == text
--- utils/random_values.py
+++ utils/random_values.py
@@ -4,9 +4,18 @@ import string
 
 def random_string(length=5, chars=string.ascii_lowercase + string.digits):
     """
-    Generate the random string
+    Generate a random string
     :param length: the length of the string generated
     :param chars: chars that are used in string generation
     :return: string
     """
     return ''.join((random.choice(chars) for x in range(length)))
+
+
+def random_number(length=5):
+    """
+    Generate a random number
+    :param length: the length of the number generated
+    :return: int
+    """
+    return int(random_string(length, chars=string.digits))
