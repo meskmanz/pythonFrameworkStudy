c136d4f7222dfbf07c8b0b138fa0c3ec3048e64f
Pavlo Varlamov <Pavlo.Varlamov@ext.us.panasonic.com>
2024-01-03 11:32:51 +0200

    Add logic to create log directory

--- utils/logger.py
+++ utils/logger.py
@@ -7,8 +7,11 @@ class Logger:
 
     @staticmethod
     def log():
-        _path = os.path.join(Path(__file__).absolute().parent.parent, 'logs', 'test_run.log')
-        logging.basicConfig(filename=_path, format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+        _path = os.path.join(Path(__file__).absolute().parent.parent, 'logs')
+        if not os.path.exists(_path):
+            os.makedirs(_path)
+        logging.basicConfig(filename=os.path.join(_path, 'test_run.log'), format='%(asctime)s: %(message)s',
+                            datefmt='%m/%d/%Y %H:%M:%S')
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
         return logger
